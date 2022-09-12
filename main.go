24e953d318b34f8475e8f70900e09b1e075eb2f7
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-09-12 20:12:44 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "github.com/PuerkitoBio/goquery"
+)
+func main(){
+  res, err := http.Get("https://www.kitapsec.com/mobil/49-gunde-tyt-matematik-kampi-eko-tonguc-akademi_urn613687.html")
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+  doc, err := goquery.NewDocumentFromReader(res.Body)
+  title := doc.Find(".dty_urunFiyati").Text()
+		fmt.Printf("Review: %s\n",title)
+	}
